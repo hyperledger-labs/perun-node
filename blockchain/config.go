5cf83f46971aaa17ba8243bcaf7a6dea5922a4ef
choeppler <16307398+choeppler@users.noreply.github.com>
2019-11-07 09:06:06 +0100

    Fix typo in copyright header (#13)
    Signed-off-by: Chris Hoeppler <christian.hoeppler@bosch.com>

--- blockchain/config.go
+++ blockchain/config.go
@@ -1,6 +1,6 @@
 // Copyright (c) 2019 - for information on the respective copyright owner
 // see the NOTICE file and/or the repository at
-//     https://github.com/direct-state-transfer/dst-go/NOTICE
+// https://github.com/direct-state-transfer/dst-go
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
