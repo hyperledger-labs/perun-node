33852c0cbf944d930ab0ffe2f8abf26bec332ef4
Manoranjith <ponraj.manoranjitha@in.bosch.com>
2020-08-21 18:01:22 +0530

    Parse and set off-chain address in user init
    Previously NewUnlockedUser was only unlocking the accounts corresponding
    to the user and initializing the wallets to access the accounts.
    
    Update it to parse and set the off-chain address as well.
    
    Signed-off-by: Manoranjith <ponraj.manoranjitha@in.bosch.com>

--- session/user.go
+++ session/user.go
@@ -29,31 +29,42 @@ func NewUnlockedUser(wb perun.WalletBackend, cfg UserConfig) (perun.User, error)
 	var err error
 	u := perun.User{}
 
-	if u.OnChain.Wallet, err = newWallet(wb, cfg.OnChainWallet, cfg.OnChainAddr); err != nil {
+	if u.OnChain, err = newCred(wb, cfg.OnChainWallet, cfg.OnChainAddr); err != nil {
 		return perun.User{}, errors.WithMessage(err, "on-chain wallet")
 	}
-	if u.OffChain.Wallet, err = newWallet(wb, cfg.OffChainWallet, cfg.OffChainAddr); err != nil {
+	if u.OffChain, err = newCred(wb, cfg.OffChainWallet, cfg.OffChainAddr); err != nil {
 		return perun.User{}, errors.WithMessage(err, "off-chain wallet")
 	}
 	if u.PartAddrs, err = parseUnlock(wb, u.OffChain.Wallet, cfg.PartAddrs...); err != nil {
 		return perun.User{}, errors.WithMessage(err, "participant addresses")
 	}
-	u.OffChainAddr = u.OffChain.Addr
-	u.CommAddr = cfg.CommAddr
-	u.CommType = cfg.CommType
+
+	u.Peer.Alias = perun.OwnAlias
+	u.Peer.CommAddr = cfg.CommAddr
+	u.Peer.CommType = cfg.CommType
+	u.Peer.OffChainAddr = u.OffChain.Addr
+	u.Peer.OffChainAddrString = u.OffChain.Addr.String()
 
 	return u, nil
 }
 
-// newWallet initializes the wallet using the wallet backend and unlocks accounts corresponding
-// to the given address.
-func newWallet(wb perun.WalletBackend, cfg WalletConfig, addr string) (pwallet.Wallet, error) {
+// newCred initializes the wallet using the wallet backend, unlocks the accounts and
+// returns the credential to access the account specified in the config.
+func newCred(wb perun.WalletBackend, cfg WalletConfig, addr string) (perun.Credential, error) {
 	w, err := wb.NewWallet(cfg.KeystorePath, cfg.Password)
 	if err != nil {
-		return nil, err
+		return perun.Credential{}, err
 	}
-	_, err = parseUnlock(wb, w, addr)
-	return w, err
+	addrs, err := parseUnlock(wb, w, addr)
+	if err != nil {
+		return perun.Credential{}, err
+	}
+	return perun.Credential{
+		Addr:     addrs[0],
+		Wallet:   w,
+		Keystore: cfg.KeystorePath,
+		Password: cfg.Password,
+	}, nil
 }
 
 // parseUnlock parses the given addresses string using the wallet backend and unlocks accounts
