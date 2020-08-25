46082a1f1988a13fc8f4c4f772c58fedbf6eb042
Manoranjith <ponraj.manoranjitha@in.bosch.com>
2020-08-25 18:09:06 +0530

    Update protocol buffer defs for user api
    Change the datatype of ClosingBalance in PayChClose API from
    []*Balanceinfo to *BalanceInfo, because Balanceinfo by itself has a map
    within it that to store the balance of different channel participants.
    
    Also changed field "Resp" to "Response" in MsgSuccess of OpenPayCh api
    to keep it consistent with other APIs.
    
    Signed-off-by: Manoranjith <ponraj.manoranjitha@in.bosch.com>

---
+++ api/grpc/server.go
@@ -0,0 +1,115 @@
+// Copyright (c) 2020 - for information on the respective copyright owner
+// see the NOTICE file and/or the repository at
+// https://github.com/hyperledger-labs/perun-node
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package grpc
+
+import (
+	"context"
+
+	psync "perun.network/go-perun/pkg/sync"
+
+	"github.com/hyperledger-labs/perun-node"
+	"github.com/hyperledger-labs/perun-node/api/grpc/pb"
+)
+
+// PayChServer represents a grpc server that implements payment channel API.
+type PayChServer struct {
+	n perun.NodeAPI
+
+	// The mutex should be used when accessing the map data structures.
+	psync.Mutex
+
+	// These maps are used to hold an signal channel for each active subscription.
+	// When a subscription is registered, subscribe function will add an entry to the
+	// map corresponding to the subscription type.
+	// The unsubscribe call should retrieve the channel from the map and close it, which
+	// will signal the subscription routine to end.
+	//
+	// chProposalsNotif & chCloseNotifs work on per session basis and hence this is a map
+	// of session id to signaling channel.
+	// chUpdatesNotif work on a per channel basis and hence this is a map of session id to
+	// channel id to signaling channel.
+
+	chProposalsNotif map[string]chan bool
+	chUpdatesNotif   map[string]map[string]chan bool
+	chClosesNotif    map[string]chan bool
+}
+
+// NewPayChServer returns a new grpc server that can server the payment channel API.
+func NewPayChServer(n perun.NodeAPI) *PayChServer {
+	return &PayChServer{
+		n:                n,
+		chProposalsNotif: make(map[string]chan bool),
+		chUpdatesNotif:   make(map[string]map[string]chan bool),
+		chClosesNotif:    make(map[string]chan bool),
+	}
+}
+
+// GetConfig wraps node.GetConfig.
+func (a *PayChServer) GetConfig(context.Context, *pb.GetConfigReq) (*pb.GetConfigResp, error) {
+	cfg := a.n.GetConfig()
+	return &pb.GetConfigResp{
+		ChainAddress:       cfg.ChainURL,
+		AdjudicatorAddress: cfg.Adjudicator,
+		AssetAddress:       cfg.Asset,
+		CommTypes:          cfg.CommTypes,
+		ContactTypes:       cfg.ContactTypes,
+	}, nil
+}
+
+// Time wraps node.Time.
+func (a *PayChServer) Time(context.Context, *pb.TimeReq) (*pb.TimeResp, error) {
+	return &pb.TimeResp{
+		Time: a.n.Time(),
+	}, nil
+}
+
+// Help wraps node.Help.
+func (a *PayChServer) Help(context.Context, *pb.HelpReq) (*pb.HelpResp, error) {
+	return &pb.HelpResp{
+		Apis: a.n.Help(),
+	}, nil
+}
+
+// OpenSession wraps node.OpenSession.
+func (a *PayChServer) OpenSession(ctx context.Context, req *pb.OpenSessionReq) (*pb.OpenSessionResp, error) {
+	errResponse := func(err error) *pb.OpenSessionResp {
+		return &pb.OpenSessionResp{
+			Response: &pb.OpenSessionResp_Error{
+				Error: &pb.MsgError{
+					Error: err.Error(),
+				},
+			},
+		}
+	}
+
+	sessionID, err := a.n.OpenSession(req.ConfigFile)
+	if err != nil {
+		return errResponse(err), nil
+	}
+
+	a.Lock()
+	a.chUpdatesNotif[sessionID] = make(map[string]chan bool)
+	a.Unlock()
+
+	return &pb.OpenSessionResp{
+		Response: &pb.OpenSessionResp_MsgSuccess_{
+			MsgSuccess: &pb.OpenSessionResp_MsgSuccess{
+				SessionID: sessionID,
+			},
+		},
+	}, nil
+}
