manoranjith/dst-go

Rename module from "dst-go" to "perun-node"
Also change the import paths in go files.

Relates to #55

Signed-off-by: Manoranjith <ponraj.manoranjitha@in.bosch.com>

blockchain/ethereum/chain.go
@@ -27,8 +27,8 @@ import (
 	ethchannel "perun.network/go-perun/backend/ethereum/channel"
 	ethwallet "perun.network/go-perun/backend/ethereum/wallet"
 
-	"github.com/direct-state-transfer/dst-go"
-	"github.com/direct-state-transfer/dst-go/blockchain/ethereum/internal"
+	node "github.com/direct-state-transfer/perun-node"
+	"github.com/direct-state-transfer/perun-node/blockchain/ethereum/internal"
 )
 
 // ChainTxTimeout is the timeout for on-chain transactions.
@@ -42,7 +42,7 @@ const ChainTxTimeout = 10 * time.Minute
 //
 // The function signature uses only types defined in root of dst project and types from std lib.
 // This enables the function to be loaded as symbol without importing this package when it is compiled as plugin.
-func NewChainBackend(url string, timeout time.Duration, cred dst.Credential) (dst.ChainBackend, error) {
+func NewChainBackend(url string, timeout time.Duration, cred node.Credential) (node.ChainBackend, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), timeout)
 	defer cancel()
 	ethereumBackend, err := ethclient.DialContext(ctx, url)

blockchain/ethereum/ethereumtest/chain.go
@@ -29,8 +29,8 @@ import (
 	ethwallet "perun.network/go-perun/backend/ethereum/wallet"
 	"perun.network/go-perun/wallet"
 
-	"github.com/direct-state-transfer/dst-go"
-	"github.com/direct-state-transfer/dst-go/blockchain/ethereum/internal"
+	node "github.com/direct-state-transfer/perun-node"
+	"github.com/direct-state-transfer/perun-node/blockchain/ethereum/internal"
 )
 
 // ChainTxTimeout is the timeout for on-chain transactions.
@@ -42,7 +42,7 @@ const ChainTxTimeout = 1 * time.Minute
 // see go-ethereum) with required contracts deployed on it and a UserSetup.
 type ChainBackendSetup struct {
 	*WalletSetup
-	ChainBackend       dst.ChainBackend
+	ChainBackend       node.ChainBackend
 	AdjAddr, AssetAddr wallet.Address
 }
 

client/client.go
@@ -28,8 +28,8 @@ import (
 	"perun.network/go-perun/peer"
 	"perun.network/go-perun/pkg/sortedkv/leveldb"
 
-	"github.com/direct-state-transfer/dst-go"
-	"github.com/direct-state-transfer/dst-go/blockchain/ethereum"
+	node "github.com/direct-state-transfer/perun-node"
+	"github.com/direct-state-transfer/perun-node/blockchain/ethereum"
 )
 
 // Client is a wrapper type around the state channel client implementation from go-perun.
@@ -43,7 +43,7 @@ type Client struct {
 // NewEthereumPaymentClient initializes a two party, ethereum payment channel client for the given user.
 // It establishes a connection to the blockchain and verifies the integrity of contracts at the given address.
 // It uses the comm backend to initialize adapters for off-chain communication network.
-func NewEthereumPaymentClient(cfg Config, user dst.User, comm dst.CommBackend) (*Client, error) {
+func NewEthereumPaymentClient(cfg Config, user node.User, comm node.CommBackend) (*Client, error) {
 	dialer, listener, err := initComm(comm, user.CommAddr)
 	if err != nil {
 		return nil, err
@@ -88,13 +88,13 @@ func (c *Client) Close() error {
 	return errors.Wrap(err, "closing channel client")
 }
 
-func initComm(comm dst.CommBackend, listenAddr string) (peer.Dialer, peer.Listener, error) {
+func initComm(comm node.CommBackend, listenAddr string) (peer.Dialer, peer.Listener, error) {
 	dialer := comm.NewDialer()
 	listener, err := comm.NewListener(listenAddr)
 	return dialer, listener, err
 }
 
-func connectToChain(cfg ChainConfig, cred dst.Credential) (channel.Funder, channel.Adjudicator, error) {
+func connectToChain(cfg ChainConfig, cred node.Credential) (channel.Funder, channel.Adjudicator, error) {
 	walletBackend := ethereum.NewWalletBackend()
 	assetAddr, err := walletBackend.ParseAddr(cfg.Asset)
 	if err != nil {

dst.go
@@ -14,7 +14,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package dst
+package node
 
 import (
 	"context"

hyperledger-labs/perun-node

blockchain/ethereum/chain.go
@@ -27,8 +27,8 @@ import (
 	ethchannel "perun.network/go-perun/backend/ethereum/channel"
 	ethwallet "perun.network/go-perun/backend/ethereum/wallet"
 
-	"github.com/direct-state-transfer/dst-go"
-	"github.com/direct-state-transfer/dst-go/blockchain/ethereum/internal"
+	"github.com/direct-state-transfer/perun-node"
+	"github.com/direct-state-transfer/perun-node/blockchain/ethereum/internal"
 )
 
 // ChainTxTimeout is the timeout for on-chain transactions.
@@ -42,7 +42,7 @@ const ChainTxTimeout = 10 * time.Minute
 //
 // The function signature uses only types defined in root of dst project and types from std lib.
 // This enables the function to be loaded as symbol without importing this package when it is compiled as plugin.
-func NewChainBackend(url string, timeout time.Duration, cred dst.Credential) (dst.ChainBackend, error) {
+func NewChainBackend(url string, timeout time.Duration, cred perun.Credential) (perun.ChainBackend, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), timeout)
 	defer cancel()
 	ethereumBackend, err := ethclient.DialContext(ctx, url)

blockchain/ethereum/ethereumtest/chain.go
@@ -29,8 +29,8 @@ import (
 	ethwallet "perun.network/go-perun/backend/ethereum/wallet"
 	"perun.network/go-perun/wallet"
 
-	"github.com/direct-state-transfer/dst-go"
-	"github.com/direct-state-transfer/dst-go/blockchain/ethereum/internal"
+	"github.com/direct-state-transfer/perun-node"
+	"github.com/direct-state-transfer/perun-node/blockchain/ethereum/internal"
 )
 
 // ChainTxTimeout is the timeout for on-chain transactions.
@@ -42,7 +42,7 @@ const ChainTxTimeout = 1 * time.Minute
 // see go-ethereum) with required contracts deployed on it and a UserSetup.
 type ChainBackendSetup struct {
 	*WalletSetup
-	ChainBackend       dst.ChainBackend
+	ChainBackend       perun.ChainBackend
 	AdjAddr, AssetAddr wallet.Address
 }
 

client/client.go
@@ -28,8 +28,8 @@ import (
 	"perun.network/go-perun/peer"
 	"perun.network/go-perun/pkg/sortedkv/leveldb"
 
-	"github.com/direct-state-transfer/dst-go"
-	"github.com/direct-state-transfer/dst-go/blockchain/ethereum"
+	"github.com/direct-state-transfer/perun-node"
+	"github.com/direct-state-transfer/perun-node/blockchain/ethereum"
 )
 
 // Client is a wrapper type around the state channel client implementation from go-perun.
@@ -43,7 +43,7 @@ type Client struct {
 // NewEthereumPaymentClient initializes a two party, ethereum payment channel client for the given user.
 // It establishes a connection to the blockchain and verifies the integrity of contracts at the given address.
 // It uses the comm backend to initialize adapters for off-chain communication network.
-func NewEthereumPaymentClient(cfg Config, user dst.User, comm dst.CommBackend) (*Client, error) {
+func NewEthereumPaymentClient(cfg Config, user perun.User, comm perun.CommBackend) (*Client, error) {
 	dialer, listener, err := initComm(comm, user.CommAddr)
 	if err != nil {
 		return nil, err
@@ -88,13 +88,13 @@ func (c *Client) Close() error {
 	return errors.Wrap(err, "closing channel client")
 }
 
-func initComm(comm dst.CommBackend, listenAddr string) (peer.Dialer, peer.Listener, error) {
+func initComm(comm perun.CommBackend, listenAddr string) (peer.Dialer, peer.Listener, error) {
 	dialer := comm.NewDialer()
 	listener, err := comm.NewListener(listenAddr)
 	return dialer, listener, err
 }
 
-func connectToChain(cfg ChainConfig, cred dst.Credential) (channel.Funder, channel.Adjudicator, error) {
+func connectToChain(cfg ChainConfig, cred perun.Credential) (channel.Funder, channel.Adjudicator, error) {
 	walletBackend := ethereum.NewWalletBackend()
 	assetAddr, err := walletBackend.ParseAddr(cfg.Asset)
 	if err != nil {

dst.go
@@ -14,7 +14,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package dst
+package perun
 
 import (
 	"context"