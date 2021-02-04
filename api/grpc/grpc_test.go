dff5b11e0a6f0721091e6228f004d9d8f9d4e10d
Manoranjith <ponraj.manoranjitha@in.bosch.com>
2021-02-04 15:53:07 +0530

    Add definitions for new api error type
    - The newly added error type will facilitate easier handling of error
      scenarios by the user of api over remote interface.
    
    - Each error will include
      - Error Category: A numeric code describes how error should be handled.
      - Error Code: A numeric code that describes specific error conditions.
        Unspecific errors are assigned code corresponding to "internal
        unknown error".
      - Message: Actual error message.
      - Additional Info: Contains additional data for handling the error as
        key value pairs.
    
    - Each of the methods in node, session and channel API will be updated
      to return errors of the newly defined type in upcoming pull requests.
    
    - Proto file have also been updated to include the same definitions.
    
    Relates to #132.
    
    Signed-off-by: Manoranjith <ponraj.manoranjitha@in.bosch.com>

--- api/grpc/grpc_test.go
+++ api/grpc/grpc_test.go
@@ -30,3 +30,30 @@ func Test_ChUpdateType(t *testing.T) {
 	assert.EqualValues(t, perun.ChUpdateTypeFinal, pb.SubPayChUpdatesResp_Notify_final)
 	assert.EqualValues(t, perun.ChUpdateTypeClosed, pb.SubPayChUpdatesResp_Notify_closed)
 }
+
+func Test_ErrorCategory(t *testing.T) {
+	assert.Equal(t, pb.ErrorCategory_ParticipantError, pb.ErrorCategory(perun.ParticipantError))
+	assert.Equal(t, pb.ErrorCategory_ClientError, pb.ErrorCategory(perun.ClientError))
+	assert.Equal(t, pb.ErrorCategory_ProtocolError, pb.ErrorCategory(perun.ProtocolFatalError))
+	assert.Equal(t, pb.ErrorCategory_InternalError, pb.ErrorCategory(perun.InternalError))
+}
+
+func Test_ErrorCode(t *testing.T) {
+	assert.Equal(t, pb.ErrorCode_ErrV2PeerResponseTimedout, pb.ErrorCode(perun.ErrV2PeerResponseTimedout))
+	assert.Equal(t, pb.ErrorCode_ErrV2RejectedByPeer, pb.ErrorCode(perun.ErrV2RejectedByPeer))
+	assert.Equal(t, pb.ErrorCode_ErrV2PeerNotFunded, pb.ErrorCode(perun.ErrV2PeerNotFunded))
+	assert.Equal(t, pb.ErrorCode_ErrV2UserResponseTimedOut, pb.ErrorCode(perun.ErrV2UserResponseTimedOut))
+	assert.Equal(t, pb.ErrorCode_ErrV2ResourceNotFound, pb.ErrorCode(perun.ErrV2ResourceNotFound))
+	assert.Equal(t, pb.ErrorCode_ErrV2ResourceExists, pb.ErrorCode(perun.ErrV2ResourceExists))
+	assert.Equal(t, pb.ErrorCode_ErrV2InvalidArgument, pb.ErrorCode(perun.ErrV2InvalidArgument))
+	assert.Equal(t, pb.ErrorCode_ErrV2FailedPreCondition, pb.ErrorCode(perun.ErrV2FailedPreCondition))
+	assert.Equal(t, pb.ErrorCode_ErrV2InvalidConfig, pb.ErrorCode(perun.ErrV2InvalidConfig))
+	assert.Equal(t, pb.ErrorCode_ErrV2ChainNodeNotReachable, pb.ErrorCode(perun.ErrV2ChainNodeNotReachable))
+	assert.Equal(t, pb.ErrorCode_ErrV2InvalidContracts, pb.ErrorCode(perun.ErrV2InvalidContracts))
+	assert.Equal(t, pb.ErrorCode_ErrV2TxTimedOut, pb.ErrorCode(perun.ErrV2TxTimedOut))
+	assert.Equal(t, pb.ErrorCode_ErrV2InsufficientBalForTx, pb.ErrorCode(perun.ErrV2InsufficientBalForTx))
+	assert.Equal(t, pb.ErrorCode_ErrV2ChainNodeDisconnected, pb.ErrorCode(perun.ErrV2ChainNodeDisconnected))
+	assert.Equal(t, pb.ErrorCode_ErrV2InsufficientBalForDeposit, pb.ErrorCode(perun.ErrV2InsufficientBalForDeposit))
+	assert.Equal(t, pb.ErrorCode_ErrV2UnknownInternal, pb.ErrorCode(perun.ErrV2UnknownInternal))
+	assert.Equal(t, pb.ErrorCode_ErrV2OffChainComm, pb.ErrorCode(perun.ErrV2OffChainComm))
+}
