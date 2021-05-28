bbbcc8458a192b66c255b21910106f7c4cdcbab7
Manoranjith <ponraj.manoranjitha@in.bosch.com>
2021-05-28 12:12:49 +0530

    Add license headers
    Signed-off-by: Manoranjith <ponraj.manoranjitha@in.bosch.com>

--- cmd/perunnodetui/paymentclient.go
+++ cmd/perunnodetui/paymentclient.go
@@ -1,3 +1,19 @@
+// Copyright (c) 2021 - for information on the respective copyright owner
+// see the NOTICE file and/or the repository at
+// https://github.com/hyperledger-labs/perun-node
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package main
 
 import (
@@ -61,7 +77,7 @@ func newOutgoingChannelProposalFn(peer, ours, theirs string) proposerFn {
 		}
 		msgErr, ok := resp.Response.(*pb.OpenPayChResp_Error)
 		if ok {
-			if msgErr.Error.Code == pb.ErrorCode_ErrRejectedByPeer {
+			if msgErr.Error.Code == pb.ErrorCode_ErrPeerRejected {
 				return false, "", balInfo{}, nil
 			}
 			return false, "", balInfo{}, errors.New(fmt.Sprintf("open request failed: %v", printAPIError(msgErr.Error)))
@@ -239,7 +255,7 @@ func sendUpdate(chID, peer, amount string) (bool, balInfo, error) {
 	}
 	msgErr, ok := resp.Response.(*pb.SendPayChUpdateResp_Error)
 	if ok {
-		if msgErr.Error.Code == pb.ErrorCode_ErrRejectedByPeer ||
+		if msgErr.Error.Code == pb.ErrorCode_ErrPeerRejected ||
 			msgErr.Error.Code == pb.ErrorCode_ErrPeerRequestTimedOut {
 			return false, balInfo{}, nil
 		}
